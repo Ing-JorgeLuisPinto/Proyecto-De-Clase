90e7c040b7c4fcc047e60a00911b7846a35e2f67
Jorge Luis Pinto Velandia <pintojorgeluis16@gmail.com>
2026-02-19 11:08:17 -0500

    Grupo de rutas

--- proyecto-clase/routes/web.php
+++ proyecto-clase/routes/web.php
@@ -6,8 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class]);
 
-Route::get('/product',  [ProducController::class, 'index']); 
-Route::get('/product/create', [ProductController::class, 'create']);
+Route::prefix('product')->controller(ProductController::class) ->group(function () {
+    Route::get('/',   'index'); 
+    Route::get('/create', 'create');
+    Route::get('/{producto}', 'show');
+});
 
 
-Route::get('/producto/{producto}', [ProductController::class, 'show']);
